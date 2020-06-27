7110d37211132aeed1e90cbbbdd432b496c1933c
thomaseyaa <61420084+thomaseyaa@users.noreply.github.com>
2020-06-27 12:15:53 +0200

    Add files via upload

--- gameAssets.py
+++ gameAssets.py
@@ -1,11 +1,11 @@
 import pygame
 import random
 
-#Fenetre du jeu
+#Fenêtre du jeu
 window = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Snake')
 
-#Varioable des couleurs
+#Varioables globales
 white = (211, 211, 211)
 black = (0, 0, 0)
 green = (58, 157, 35)
@@ -13,7 +13,6 @@ red = (255, 0, 0)
 yellow = (255, 255, 0)
 pink = (255, 192, 203)
 
-#Variable des positions
 left = 250
 top = 250
 leftDirection = 0
@@ -21,13 +20,13 @@ topDirection = 0
 
 score = 0
 
-#Fonction couleur du fruit
+#Fonction pour la couleur du fruit
 def fruitColor():
     fruitColor = [red,yellow,pink]
     x = random.randrange(0,3)
     return fruitColor[x]
 
-#Fonction grille du jeu
+#Fonction pour dessiner une grille
 def grid():
     numberOfColumn = 0
     topLign = 0
